773c09ba489328f18b7b03f50c4fec271ff28902
georgecmf <gemarquesf@gmail.com>
2026-04-13 20:01:31 -0300

    fix: removendo tema dark e corrigindo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import Projetos from './containers/Projetos'
-import Sidebar from './containers/Sidebar'
 import Hero from './containers/Hero'
 import Beneficios from './components/Beneficios'
 import Sobre from './containers/Sobre'
@@ -13,27 +11,20 @@ import ComoFunciona from './containers/ComoFunciona'
 import CTA from './containers/CTA'
 import EstiloGlobal, { Container } from './styles'
 import temaLight from './themes/light'
-import temaDark from './themes/dark'
 
 function App() {
-  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
-
-  function trocaTema() {
-    setEstaUsandoTemaDark(!estaUsandoTemaDark)
-  }
-
   return (
-    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
+    <ThemeProvider theme={temaLight}>
       <EstiloGlobal />
       <Container>
         <main>
           <Hero />
-          <Beneficios/>
+          <Beneficios />
           <Sobre />
           <Projetos />
           <Servicos />
-          <Depoimentos/>
-          <FAQ/>
+          <Depoimentos />
+          <FAQ />
           <ComoFunciona />
           <CTA />
         </main>
